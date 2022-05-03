df411e9c55594f85555f3380895d0d6adae5c45a
jeffmlb91 <jeffmlb91@gmail.com>
2022-05-03 19:45:54 -0400

    Inital commit: set softeare environement

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+
 import './App.css';
+import SocialCard from './SocialCard'
 
 function App() {
+
+  //The useState function returns an array which contains two items: item (users) and setItem (setUsers);
+  //useEffect takes a function which can contain any kind of operation including side effects...any kind of side effect should be used inside useEffect
+  const [users, setUsers] = useState([]);
+  
+  useEffect(() => {
+    (async () => {
+      let userData;
+      try{
+       const response = await fetch('https://randomuser.me/api/?results=10'); 
+       const userData = await response.json().results;
+      } catch (err) {
+        console.log(err);
+        userData = [];
+      }
+      //setItem or setUser in this case will, is an updater function which will be responsible to update the item or in this case users simple as that.
+      setUsers(userData);
+
+    })()
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.map((user, index) => (
+        <SocialCard userData={user} key={index}/>
+      ))}
     </div>
   );
 }
